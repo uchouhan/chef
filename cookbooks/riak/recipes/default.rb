dac260e1cfcfee6f7ccff1ca54cd4f24f6303e8e
Umang Chouhan <uchouhan@optimiscorp.com>
2011-08-17 14:54:03 -0700

    Fixing the version of riak.

--- cookbooks/riak/recipes/default.rb
+++ cookbooks/riak/recipes/default.rb
@@ -1,3 +1,3 @@
 execute 'Install Riak' do
-  command 'cd /usr/local && curl -O http://downloads.basho.com/riak/riak-0.14/riak-0.14.2-osx-i386.tar.gz && tar xzvf riak-0.14.2-osx-i386.tar.gz'
+  command 'cd /usr/local && curl -O http://downloads.basho.com/riak/riak-0.14/riak-0.14.0-osx-i386.tar.gz && tar xzvf riak-0.14.0-osx-i386.tar.gz'
 end
