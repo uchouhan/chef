76b3216e3abf2fb77b12228f6bbd50bc4b04e841
Umang Chouhan <uchouhan@optimiscorp.com>
2011-08-17 09:34:32 -0700

    Adding a rake configuration.

---
+++ config/rake.rb
@@ -0,0 +1,9 @@
+COMPANY_NAME = 'Theriffer Inc.'
+SSL_COUNTRY_NAME = 'US'
+SSL_STATE_NAME = 'California'
+SSL_LOCALITY_NAME = 'Pacific Palisades'
+SSL_ORGANIZATIONAL_UNIT_NAME = 'Operations'
+SSL_EMAIL_ADDRESS = 'theriffer@me.com'
+NEW_COOKBOOK_LICENSE = :apachev2
+TOPDIR = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+CADIR = File.expand_path(File.join(TOPDIR, 'certificates'))
